42046dba0afb117020627ac58e6ae9f4be398bc4
Ольга <lelya.zy@gmail.com>
2023-03-05 22:39:48 +0300

    Определены основные функции программы и интерфейс для пользователя

---
+++ Controller.py
@@ -0,0 +1,7 @@
+class Controller:
+        
+    def __init__(self, Function, Interface):
+        self.function = Function
+        self.interface = Interface
+
+    def menu():
--- Function.py
+++ Function.py
@@ -1,1 +1,15 @@
+class Function:
 
+    def write_json():
+        
+    def read_notes():
+        
+    def add_note():
+        
+    def sort_notes():
+        
+    def del_note():
+        
+    def del_notes():
+        
+    def edit_note():
